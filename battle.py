a1efd62649de1dece4d0e2bfd341042c42805fed
Adam Melancon <adammelancon@gmail.com>
2021-03-17 15:21:10 -0500

    Update battle.py
    Updated to work on Python 3.8 and up.

--- battle.py
+++ battle.py
@@ -6,37 +6,36 @@ import os
 os.system("clear")
 
 def battle(n):
-    pokemon1 = raw_input("Player 1's Pokemon Name? ")
-    pokemon2 = raw_input("Player 2's Pokemon Name? ")
+    pokemon1 = input("Player 1's Pokemon Name? ")
+    pokemon2 = input("Player 2's Pokemon Name? ")
     pokepower1 = random.randint(1,100)
     pokepower2 = random.randint(1,100)
-    print 'Pokemon are doing battle! \n',
+    print ('Pokemon are doing battle! \n')
     pokepower1 = random.randint(1,100)
     pokepower2 = random.randint(1,100)
     for i in range(n):
         time.sleep(.25)
-        print '\b.....' * 5,
+        print ('.....')
         sys.stdout.flush()
-    print '\nThe fight is finished!'
-    raw_input("Press Enter to Continue.")
-    print "\n"
-    print "Name:" + " " + pokemon1 + "   " + "Power:" + " " + str(pokepower1)
-    print "Name:" + " " + pokemon2 + "   " + "Power:" + " " + str(pokepower2)
-    print "\n"
+    print ('\nThe fight is finished!')
+    input("Press Enter to Continue.")
+    print ("\n")
+    print ("Name:" + " " + pokemon1 + "   " + "Power:" + " " + str(pokepower1))
+    print ("Name:" + " " + pokemon2 + "   " + "Power:" + " " + str(pokepower2))
+    print ("\n")
     if pokepower1 == pokepower2:
-        print "It's a tie!"
+        print ("It's a tie!")
     elif pokepower1 > pokepower2:
-        print pokemon1 +  " " + "WINS THE BATTLE!"
+        print (pokemon1 +  " " + ("WINS THE BATTLE!"))
     else:
-        print pokemon2 +  " " + "WINS THE BATTLE!"
+        print (pokemon2 +  " " + ("WINS THE BATTLE!"))
 
-    print "\n"
-    print "Enter 'B' to battle again, or 'E' to exit"
-    if raw_input().lower() == "b":
+    print ("\n")
+    print ("Enter 'B' to battle again, or 'E' to exit")
+    if input().lower() == "b":
         os.system("clear")
         battle(10)
     else:
         exit
-
+        
 battle(10)
-
